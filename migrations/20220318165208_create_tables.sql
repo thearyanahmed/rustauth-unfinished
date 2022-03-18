10f49f241ab2305a67de64056f40af1691208b09
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-03-18 22:58:18 +0600

    Added docker compose for postgres

---
+++ migrations/20220318165208_create_tables.sql
@@ -0,0 +1,12 @@
+-- Add migration script here
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+create table users(
+    id uuid default uuid_generate_v4() primary key,
+    username varchar not null unique,
+    email varchar not null unique,
+    password varchar not null,
+    name varchar null,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp,
+);
